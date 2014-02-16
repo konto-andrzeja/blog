49bf2f7a8babcec3e20936caa6773d1196b926e1
konto-andrzeja <e43e43@gmail.com>
2014-02-16 12:08:32 +0100

    add a simple comment system

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
+  expose(:post)
+  expose(:comment, attributes: :comment_params)
+  expose(:comments, ancestor: :post)
+
+  def create
+    comment.user = current_user
+    if comment.save
+      redirect_to post
+    else
+      redirect_to post, notice: "Your comment couldn't be added. You probably didn't write anything."
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
