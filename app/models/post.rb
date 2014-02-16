834097694f9ae8679277c57ff018664c573ad674
konto-andrzeja <e43e43@gmail.com>
2014-02-16 13:03:27 +0100

    making specs green - hotness

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,20 @@ class Post
     update_attribute :archived, true
   end
 
+  def hotness
+    if comments.size < 3
+      hotness = 0
+    else
+      hotness = 1
+    end
+    if created_at > 1.days.ago
+      hotness + 3
+    elsif created_at > 3.days.ago
+      hotness + 2
+    elsif created_at > 7.days.ago
+      hotness + 1
+    else
+      hotness
+    end
+  end
 end
